7534e2362e49d5430e38b90214083a62d449735b
siam <rifadul5@gmail.com>
2025-07-05 18:42:06 +0600

    Refactor code structure for improved readability and maintainability

--- src/app/layouts/index.jsx
+++ src/app/layouts/index.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router';
-import Footer from '../../components/footer';
-import Header from '../../components/header';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function RootLayout() {
     return (
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import jsconfigPaths from "vite-jsconfig-paths";
+import eslint from "vite-plugin-eslint";
+import svgr from "vite-plugin-svgr";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
@@ -7,7 +10,7 @@ export default defineConfig({
   theme: {
     extend: {},
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), jsconfigPaths(), svgr(), eslint(), tailwindcss()],
   server: {
     port: 3000,
   },
